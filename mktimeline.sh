a47d594a3d1dba999d996d0e5d2ff80d24ac60f6
Michael Arntzenius <daekharel@gmail.com>
2013-12-31 15:38:49 +0100

    timeline changes

--- mktimeline.sh
+++ mktimeline.sh
@@ -2,6 +2,6 @@
 egrep -v '^(#.*)?$' | while read -d : date; do
     read event
     # NB. replacing hyphen-minus with unicode minus
-    echo -n "<tr><td>${date}</td>" | sed 's/ /\&nbsp;/g;s/-/−/g'
+    echo -n "<tr><td>${date}</td>" | sed -re 's/c\. /c\.\&nbsp;/g;s/-/−/g'
     echo "<td>${event}</td></tr>"
 done
